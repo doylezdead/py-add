494c558618f3af90caaaa6f988746734aba38567
Ryan Doyle <rcdoyle@mtu.edu>
2016-08-08 01:05:03 -0700

    basic functions and doesnt look too bad

--- py_add/command_line.py
+++ py_add/command_line.py
@@ -16,11 +16,19 @@ class CLI(object):
 
     def __init__(self):
         self.motd = 'Welcome :)'
-        control_actions = {
+        self.control_actions = {
             'q': self.end,
             'w': self.write,
+            'r': self.revert,
             '[': self.step_back,
-            ']': self.cancel_lookback()
+            ']': self.cancel_lookback
+        }
+
+        self.op_actions = {
+            '+': self.op_add,
+            '-': self.op_sub,
+            '*': self.op_mult,
+            '/': self.op_div,
         }
 
         #start setting up terminal
@@ -30,24 +38,67 @@ class CLI(object):
 
     def run(self):
         count = 0;
-        sys.stdout.write('   ' + '=' * (self.width - 6) + '\n\n   ' + '=' * (self.width - 6) + '{}\r'.format(UP))
+        self.stack.push(val=0)
+        #self.stack.push(op='+', val=100, result=200)
+        #self.stack.push(op='+', val=100, result=300)
+        #self.stack.push(op='+', val=100, result=400)
+        #self.stack.push(op='+', val=1000, result=1400)
+        #self.stack.push(op='-', val=100, result=1300)
+        #self.stack.push(op='+', val=100, result=1400)
+
+
 
         while self.running:
-            sys.stdout.write(' ' * self.width + '\r   >{}> '.format(count))
+            new_out = self.stack.print_tape(buffer=2, width=self.width - 2, border=True)
+            num_lines = len(new_out.split('\n'))
+            sys.stdout.write(UP*(num_lines+2) + new_out + '\n')
+            sys.stdout.write('  ' + '=' * (self.width - 4) + '\n\n  ' + '=' * (self.width - 4) + '{}\r'.format(UP))
+            sys.stdout.write(' ' * self.width + '\r  >{}> '.format(count))
             sys.stdout.flush()
             input_str = input()
             count+=1
-            sys.stdout.write('   ' + '=' * (self.width - 6) + '{}\r'.format(UP))
+
+            self.parse_in(input_str)
 
         sys.stdout.flush()
         print('\n\nThank you for your patronage')
         return 0
 
+    def parse_in(self, in_str):
+        try:
+            if in_str[0] in self.control_actions:
+                self.control_actions[in_str[0]]()
+
+            elif in_str[0] in self.op_actions:
+               self.op_actions[in_str[0]](float(in_str[1:]))
+            else:
+                self.op_add(float(in_str))
+        except Exception:
+            pass
+
+    def op_add(self, input_val):
+        self.stack.push(op='+', val=input_val, result=self.stack.get_current_val()+input_val)
+
+    def op_sub(self, input_val):
+        self.stack.push(op='-', val=input_val, result=self.stack.get_current_val()-input_val)
+
+    def op_mult(self, input_val):
+        self.stack.push(op='x', val=input_val, result=self.stack.get_current_val()*input_val)
+
+    def op_div(self, input_val):
+        self.stack.push(op='/', val=input_val, result=self.stack.get_current_val()/input_val)
+
+
     def write(self):
         pass
 
     def step_back(self):
-        pass
+        self.stack.step_back()
+
+    def revert(self):
+        self.stack.revert_to()
+        sys.stdout.write(UP*(self.height-4) + (' '*self.width + '\n') * (self.height-2))
+        sys.stdout.flush()
 
     def cancel_lookback(self):
         self.stack.reset()
--- py_add/lib.py
+++ py_add/lib.py
@@ -5,9 +5,9 @@ class OperationsStack():
     head_node = None
     step_node = head_node
 
-    def push(self, op='', val=0, result=0):
-        head_node = StackNode(next_node=self.head_node, op=op, val=val)
-        step_node = head_node
+    def push(self, op=' ', val=0, result=0):
+        self.head_node = StackNode(prev_node=self.head_node, op=op, val=val, result=result)
+        self.step_node = self.head_node
 
     def get_current_val(self):
         return self.step_node.result
@@ -21,30 +21,40 @@ class OperationsStack():
     def reset(self):
         self.step_node = self.head_node
 
-    def print_tape(self, width=-1, length=-1, step=False, border=False):
+    # I call it "formatting hell"
+    def print_tape(self, buffer=0, width=-1, length=-1, step=False, border=False):
         current_node = self.head_node
         printout = ''
-        prefix = ''
+        prefix = ' '*buffer
+        suffix = ''
         if border and (width==-1):
             border = False
         if border:
-            printout = '='*width + '\n'
-            prefix = '| '
-            suffix = '  |'
+
+            prefix += '| '
+            suffix = '|'
+
+
         while current_node is not None:
             indicator = ' '
             if current_node is self.step_node:
                 indicator = '>'
-            temp = ''
-            temp = '{0}{1}'.format(indicator, current_node)
-            if (temp + 6) > width:
-                temp = temp[0:-3] + '...'
+            temp = '{0} {1}'.format(indicator, current_node)
+            if border and ((len(temp) + 5) > width):
+                print(width-3-2-2)
+                temp = temp[0:(width-3-2-2)] + '..'
 
-            printout = '{0}{1}{2}\n'.format(prefix,temp,suffix)
+            printout = '{0}{1}{2}{3}\n'.format(prefix, temp, ' '*(width-3-len(temp)), suffix) + printout
 
             current_node = current_node.prev_node
 
-        if border: printout += '='*width
+        result = '{}'.format(self.step_node.result)
+        if len(result)+6 > width:
+            result = result[0:(width-6-2)] + '..'
+
+        if border:
+            printout = ' '*buffer + '-'*width + '\n' + printout + ' '*buffer + '-'*width + '\n' + ' '*buffer + '|    ' + result + '{}|\n'.format(' '*(width-len(result)-6)) + ' '*buffer + '-'*width
+
         return printout
 
 
@@ -60,7 +70,7 @@ class StackNode():
             self.result = self.val
 
     def __str__(self):
-        retstr = '{0}{1}'.format(( self.op + ' '*(2-len(self.op)) ),self.val)
+        retstr = '{0}{1}'.format(self.op,self.val)
         # if self.result is not None:
         #    retstr += ' = {}'.format(self.result)
         return retstr
